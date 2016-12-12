b6a000a537e70da430def8c7df06788be111287e
magnattic <attic@fscl.eu>
2016-12-12 20:39:28 +0100

    table layout, ppm

--- src/app/table-entry.ts
+++ src/app/table-entry.ts
@@ -1,10 +1,10 @@
 export class TableEntry {
   constructor(public playerName: string,
-              public wins: number,
-              public draws: number,
-              public losses: number,
-              public goalsScored: number,
-              public goalsConceived: number) { }
+    public wins: number,
+    public draws: number,
+    public losses: number,
+    public goalsScored: number,
+    public goalsConceived: number) { }
 
   public get goalDifference() {
     return this.goalsScored - this.goalsConceived;
@@ -16,5 +16,12 @@ export class TableEntry {
 
   public get played() {
     return this.wins + this.draws + this.losses;
+  }
+
+  public get pointsPerMatch() {
+    if (this.played === 0) {
+      return 0;
+    }
+    return Math.round( this.points / this.played * 100) / 100;
   }
 }
