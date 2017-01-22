b752c163cc5e4e2b9e4aa462069a9323e34d91b2
magnattic <attic@fscl.eu>
2017-01-22 15:31:41 +0100

    http for fixtures

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: '/league-manager/',
-  fixturesUrl: 'https://league.nuszpl.de/fixtures.json'
+  fixturesUrl: 'http://league.nuszpl.de/fixtures.json'
 };
